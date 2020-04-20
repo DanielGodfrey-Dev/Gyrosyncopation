efedc1f9aef9e6262d4a63c0088b58f86189516c
Daniel Godfrey <DanielGodfrey-Dev@outlook.com>
2020-04-19 22:59:22 -0700

    Implement spatial interaction logic for nodes
    when nodes overlap physically, able to call function to operate interactions.

--- client/components/game.jsx
+++ client/components/game.jsx
@@ -8,15 +8,14 @@ const getRandomLocation = () => {
     let min = 0;
     let max = 95;
 
-    let x = Math.random() * (max - min) + min; 
-    let y = Math.random() * (max - min) + min;
+    let x = Math.floor(Math.random() * (max - min) + min); 
+    let y = Math.floor(Math.random() * (max - min) + min);
 
     return [x, y];
 };
 
 let initialState = {
     keypadDirection: '',
-
     selfNode: [0,0],
     friendNodes: [getRandomLocation()]
 }
@@ -29,17 +28,25 @@ class Game extends React.Component {
         this.onKeyDown = this.onKeyDown.bind(this);
         this.checkGeoFence = this.checkGeoFence.bind(this);
         this.gameOver = this.gameOver.bind(this);
+        this.interaction = this.interaction.bind(this);
     }
 
     componentDidMount() {
+        //initialize keypress functionality
         document.onkeydown = this.onKeyDown;
     }
 
+
     componentDidUpdate() {
+        //always check to see if selfNode has broken Geo Fence
         console.log(this.state.selfNode, this.state.friendNodes);
         this.checkGeoFence();
+        this.interaction();
     }
 
+
+    //________________Game Over on Geo Fence Break Logic_______________
+    //checks to see if selfNode breaks geofence boundary
     checkGeoFence() {
         let selfNode = this.state.selfNode;
         if (selfNode[0] >= 98 || selfNode[1] >= 96 || selfNode[0] <= -3 || selfNode[1] <= -3) {
@@ -49,10 +56,15 @@ class Game extends React.Component {
 
     gameOver() {
         alert('Node Destruction Through the Geofence. Game Over.');
+        //________________reset game____________________________________
         this.setState(initialState);
         this.setState({ friendNodes: [getRandomLocation()]})
+        //______________________________________________________________
     }
+    //__________________________________________________________________
 
+
+    //_________________Enable control of selfNode through keyboard______
     onKeyDown(e) {
         e = e || window.event;
         switch (e.keyCode) {
@@ -82,6 +94,19 @@ class Game extends React.Component {
               break;
           }
     }
+    //__________________________________________________________________
+
+
+    //________________selfNode can interact with other nodes!___________
+    interaction() {
+        let selfNode = this.state.selfNode;
+        let friendNode = this.state.friendNodes[0];
+
+        if (selfNode[0] >= (friendNode[0] - 3) && selfNode[0] <= (friendNode[0] + 3) && selfNode[1] >= (friendNode[1] - 3) && selfNode[1] <= (friendNode[1] + 3)) {
+            console.log('node immersive interfacing initiated...');
+        }
+    }
+
 
     render() {
 
